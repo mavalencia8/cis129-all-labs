4fa81d3ceada958c942f8354fa446923ad5d0713
mavalencia8 <159061581+mavalencia8@users.noreply.github.com>
2024-02-12 12:24:04 -0700

    Update cis129_lab02_shortStory.py
    lab-2-test

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello,World!')
+# michael valencia lab-2
+#greetings
+print('good-morning')
+#market report
+print('there was three houses for sale')
+#sight
+print('there was 2 dogs')
+#afternoon
+print('good evening')
